68291c2fdfee5371538e8937f8dd0828b56a2b15
Hayden D’Souza <haydenfds@gmail.com>
2023-07-29 20:32:23 -0700

    Added path aliasing for src subdirectories w/ path resolution configs

--- client/craco.config.js
+++ client/craco.config.js
@@ -4,6 +4,12 @@ module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@context': path.resolve(__dirname, 'src/context'),
+      '@types': path.resolve(__dirname, 'src/types'),
+      '@utils': path.resolve(__dirname, 'src/utils'),    
     },
   },
   typescript: {

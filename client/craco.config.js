53ec185957b3b949f3e00687a34febd105ade414
Hayden D’Souza <haydenfds@gmail.com>
2023-07-31 09:32:32 -0700

    Added path aliasing for context subdir w/ barrel exports setup

--- client/craco.config.js
+++ client/craco.config.js
@@ -7,7 +7,7 @@ module.exports = {
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@pages': path.resolve(__dirname, 'src/pages'),
-      '@store': path.resolve(__dirname, 'src/store'),
+      '@context': path.resolve(__dirname, 'src/context'),
       '@types': path.resolve(__dirname, 'src/types'),
       '@utils': path.resolve(__dirname, 'src/utils'),    
     },

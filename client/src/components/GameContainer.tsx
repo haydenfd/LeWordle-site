32ac6be06e5e29b0309deb74672c457cd82d54e5
Hayden D’Souza <haydenfds@gmail.com>
2023-07-31 08:15:20 -0700

    Added headlessui for combobox instead b/c it fit use-case, Added filtering logic w/ fuse, rendering dropdown properly with animations

--- client/src/components/GameContainer.tsx
+++ client/src/components/GameContainer.tsx
@@ -1,22 +1,66 @@
 import { buildEndpoint, endpointsList } from '@/api'
-import React, {useEffect, useState, useRef} from 'react'
+import React, {Fragment, useEffect, useState, ChangeEvent } from 'react'
 import axios from 'axios'
 import { fuseOptions } from '@/utils'
 import Fuse from 'fuse.js'
+import { Combobox } from "@headlessui/react"
 
+type ActivePlayerDataSingleInterface = {
+  _id: number,
+  first_name: string,
+  last_name: string,
+  full_name: string
+}
+
+/* 
+  Mouse event input handler type for when the user chooses an option from dropdown menu
+*/
+
+type InputMouseEventType = ChangeEvent<HTMLInputElement>
 
 const GameContainer = () => {
 
-  const [allPlayers, setAllPlayers] = useState<any>([])
+  const [allPlayers, setAllPlayers] = useState<Array<ActivePlayerDataSingleInterface>>([])
 
+  const [suggestedPlayers, setSuggestedPlayers] = useState<Array<ActivePlayerDataSingleInterface>>([])
+
+  const [selected, setSelected] = useState<ActivePlayerDataSingleInterface | null>(null)
+
+  const [searchInput, setSearchInput] = useState<string>('')
   
   let fuzzySearchFilter = (search: string) => 
   {
     let fuse = new Fuse(allPlayers, fuseOptions)
     let results = fuse.search(search).splice(0,5)
-    console.log(results)
   }
 
+  const handleSearchInputChange = (e:InputMouseEventType) => 
+  {
+    setSearchInput(e.target.value)
+  }
+
+  useEffect(() => {
+
+    const recommendPlayers = () => {
+      const fuse = new Fuse(allPlayers, fuseOptions)
+      let fuseResults = fuse.search(searchInput).splice(0,5)
+      
+      let players:Array<ActivePlayerDataSingleInterface> = []
+      for (let i = 0; i < fuseResults.length; i++)
+      {
+        players.push(fuseResults[i].item)
+      }
+
+      setSuggestedPlayers(players)
+    }
+    
+
+    if (searchInput.length > 0)
+    {
+      recommendPlayers()
+    }
+
+  }, [searchInput])
 
   useEffect(() => {
     
@@ -34,10 +78,55 @@ const GameContainer = () => {
 
   }, [])
 
+  let [x, y] = useState<number>(1)
 
+  useEffect(() => {
+    y(x + 1)
+    setSearchInput('')
+  }, [selected])
+  
   return (
-    <div className='min-h-[60vh] text-center bg-red-400'>
-      GameContainer
+    <div className='min-h-[60vh] text-center'>
+
+      <Combobox as={Fragment} value={selected} onChange={(e:ActivePlayerDataSingleInterface) => {
+        setSelected(e)
+        console.log(e)
+      }
+      }>
+
+      {({ activeOption }) => (
+        <>
+          <Combobox.Input as={Fragment}>
+          <input placeholder={`Guess ${x} of 7`}
+              className="font-semibold border-2 border-lakerPurple w-3/5 md:w-2/5 m-auto p-2 mt-6 focus:outline-none focus:border-lakerGold md:text-lg"
+              value={searchInput} 
+              type="text"
+              onChange={handleSearchInputChange}
+              autoComplete="false"
+          />
+          </Combobox.Input>
+          {
+            suggestedPlayers.length > 0 && (
+                <Combobox.Options className="mx-auto text-left w-3/5 md:w-2/5 font-semibold border-b-2 border-x-2 border-lakerGold">
+                  {
+                    suggestedPlayers.map((player:ActivePlayerDataSingleInterface) => (
+                      <Combobox.Option
+                      key={player._id} 
+                      value={player}
+                      className={`py-1 pl-2 cursor-pointer ${
+                        activeOption === player ? 'bg-lakerGold' : 'bg-white'
+                      }`}>
+                        {player.full_name}
+                      </Combobox.Option> 
+                    )) 
+                  }
+                </Combobox.Options>
+              )
+            }
+        </>
+      )}
+
+      </Combobox>
     </div>
   )
 }

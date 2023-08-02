414f54b6a2cc6adf4ffa1d0462228e8e63c736b2
Hayden D’Souza <haydenfds@gmail.com>
2023-08-02 02:36:39 -0700

    Added state logic to handle opening and closing of modals on navbar component, need to design modals

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,29 +1,45 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {MyDialog} from './Modals/HelpModal'
 
 export const Navbar = () => {
 
-  const [isModalOpen, setModalOpen] = useState(false);
 
-  const handleModalToggle = () => {
-    setModalOpen((prev) => !prev); // Toggle the modal state
-  };
+  const [isTeamHelpModalOpen, setIsTeamHelpModalOpen] = useState<boolean>(false)
+  const [isGuideModalOpen, setIsGuideModalOpen] = useState<boolean>(false)
+  const [isStatsModalOpen, setIsStatsModalOpen] = useState<boolean>(false)
 
 
+  const handleIsTeamModalOpen = () : void => setIsTeamHelpModalOpen((prev) => !prev)
+  const handleIsGuideModalOpen = () : void => setIsGuideModalOpen((prev) => !prev)
+  const handleIsStatsModalOpen = () : void => setIsStatsModalOpen((prev) => !prev)
+
+
+  // useEffect(() => console.log(isTeamHelpModalOpen), [isTeamHelpModalOpen])
+ 
   return (
     <nav className='w-full bg-lakerPurple py-2 mb-4 flex justify-center gap-12'>
-      <button className='font-normal md:text-lg transition-all ease-in-out text-md bg-transparent text-white hover:text-lakerGold hover:scale-125 hover:font-semibold'>
+
+      <button 
+      onClick={() => handleIsGuideModalOpen()}
+      className='font-normal md:text-lg transition-all ease-in-out text-md bg-transparent text-white hover:text-lakerGold hover:scale-125 hover:font-semibold'
+      >
         How To Play
       </button>
+
       <button 
-      onClick={handleModalToggle}
-      className='font-normal md:text-lg transition-all ease-in-out text-md bg-transparent text-white hover:text-lakerGold hover:scale-125 hover:font-semibold'>
+      onClick={() => handleIsTeamModalOpen()}
+      className='font-normal md:text-lg transition-all ease-in-out text-md bg-transparent text-white hover:text-lakerGold hover:scale-125 hover:font-semibold'
+      >
         Teams Help
       </button>
-      <MyDialog open={isModalOpen} onClose={handleModalToggle} />
-      <button className='font-normal md:text-lg transition-all ease-in-out bg-transparent text-md text-white hover:text-lakerGold hover:font-semibold hover:scale-125'>
+
+      <button 
+      onClick={() => handleIsStatsModalOpen()}
+      className='font-normal md:text-lg transition-all ease-in-out bg-transparent text-md text-white hover:text-lakerGold hover:font-semibold hover:scale-125'
+      >
         Stats
       </button>
+      {/* <MyDialog open={isModalOpen} onClose={handleModalToggle} /> */}
     </nav>
   )
 }
